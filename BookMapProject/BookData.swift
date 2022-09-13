b3c8891f48717a618d5bef72367578aaf2cac012
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-09-13 16:16:42 +0900

    Place API 로 독립서점 정보 통신 완료

---
+++ BookMapProject/BookData.swift
@@ -0,0 +1,10 @@
+//
+//  BookData.swift
+//  BookMapProject
+//
+//  Created by 신승아 on 2022/09/13.
+//
+
+import Foundation
+
+
--- BookMapProject/ViewController.swift
+++ BookMapProject/ViewController.swift
@@ -6,13 +6,36 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchPlace(query: "최인아책방")
     }
+    
+    func searchPlace(query: String) {
+        let urlString = "\(EndPoint.placeURL)query=\(query)"
+        let headers: HTTPHeaders = ["X-Naver-Client-Id": APIKey.clientID, "X-Naver-Client-Secret": APIKey.clientSecret]
+        let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        let url = URL(string: encodedString)!
+        
+        
+        AF.request(url, method: .get, headers: headers).validate().responseData { response in
+            switch response.result {
+                    case .success(let value):
+                        let json = JSON(value)
+                        print("JSON: \(json)")
+
+                       
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+    }
+    
 
 
 }
