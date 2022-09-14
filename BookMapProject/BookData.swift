07bf0ee0facc5f56cc159058b4e543a8e0608a7c
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-09-14 19:07:40 +0900

    위도, 경도 double 로 변경

--- BookMapProject/BookData.swift
+++ BookMapProject/BookData.swift
@@ -17,43 +17,43 @@ class dummyData {
             "storeName": "스페인책방",
             "address": "서울특별시 중구 퇴계로36길 29 기남빌딩 5층 603호",
             "link": "http://www.spainbookshop.com",
-            "mapX": 311413,
-            "mapY": 551403
+            "mapX": 37.560543,
+            "mapY": 126.995058
             },
             {
             "storeName": "최인아책방",
             "address": "서울특별시 강남구 선릉로 521",
             "link": "http://instagram.com/inabooks",
-            "mapX": 316025,
-            "mapY": 545304
+            "mapX": 37.506026,
+            "mapY": 127.047922
             },
             {
             "storeName": "이후북스",
             "address": "서울특별시 마포구 망원로4길 24 2층",
             "link": "http://www.afterbookshop.com",
-            "mapX": 303179,
-            "mapY": 550947
+            "mapX": 37.555602,
+            "mapY": 126.901935
             },
             {
             "storeName": "스토리지북앤필름",
             "address": "서울특별시 용산구 신흥로 115-1",
             "link": "http://www.storagebookandfilm.com",
-            "mapX": 310341,
-            "mapY": 549661
+            "mapX": 37.544730,
+            "mapY": 126.983092
             },
             {
             "storeName": "땡스북스",
             "address": "서울특별시 마포구 양화로6길 57-6",
             "link": "http://www.thanksbooks.com/",
-            "mapX": 304560,
-            "mapY": 550185
+            "mapX": 37.548881,
+            "mapY": 126.917711
             },
             {
             "storeName": "유어마인드",
             "address": "서울특별시 서대문구 연희동 132-32 2층 우측",
             "link": "https://your-mind.com/",
-            "mapX": 305703,
-            "mapY": 552339
+            "mapX": 37.568435,
+            "mapY": 126.930321
             }
         ]
         """
@@ -62,8 +62,8 @@ class dummyData {
             var storeName: String
             var address: String
             var link: String
-            var mapX: Int
-            var mapY: Int
+            var mapX: Double
+            var mapY: Double
         }
         
         guard let data = jsonData.data(using: .utf8) else {
