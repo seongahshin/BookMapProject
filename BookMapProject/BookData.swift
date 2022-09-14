006ac625b319a7c9ce53b6c93af3d9db4f005ef2
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-09-14 13:44:55 +0900

    Place API 사용 대신 독립서점 json Data 만들기

--- BookMapProject/BookData.swift
+++ BookMapProject/BookData.swift
@@ -8,3 +8,78 @@
 import Foundation
 
 
+class dummyData {
+    
+    func decode() {
+        let jsonData = """
+        [
+            {
+            "storeName": "스페인책방",
+            "address": "서울특별시 중구 퇴계로36길 29 기남빌딩 5층 603호",
+            "link": "http://www.spainbookshop.com",
+            "mapX": 311413,
+            "mapY": 551403
+            },
+            {
+            "storeName": "최인아책방",
+            "address": "서울특별시 강남구 선릉로 521",
+            "link": "http://instagram.com/inabooks",
+            "mapX": 316025,
+            "mapY": 545304
+            },
+            {
+            "storeName": "이후북스",
+            "address": "서울특별시 마포구 망원로4길 24 2층",
+            "link": "http://www.afterbookshop.com",
+            "mapX": 303179,
+            "mapY": 550947
+            },
+            {
+            "storeName": "스토리지북앤필름",
+            "address": "서울특별시 용산구 신흥로 115-1",
+            "link": "http://www.storagebookandfilm.com",
+            "mapX": 310341,
+            "mapY": 549661
+            },
+            {
+            "storeName": "땡스북스",
+            "address": "서울특별시 마포구 양화로6길 57-6",
+            "link": "http://www.thanksbooks.com/",
+            "mapX": 304560,
+            "mapY": 550185
+            },
+            {
+            "storeName": "유어마인드",
+            "address": "서울특별시 서대문구 연희동 132-32 2층 우측",
+            "link": "https://your-mind.com/",
+            "mapX": 305703,
+            "mapY": 552339
+            }
+        ]
+        """
+
+        struct BookData: Decodable {
+            var storeName: String
+            var address: String
+            var link: String
+            var mapX: Int
+            var mapY: Int
+        }
+        
+        guard let data = jsonData.data(using: .utf8) else {
+            fatalError()
+        }
+        
+        do {
+            let value = try JSONDecoder().decode([BookData].self, from: data)
+            dump(value)
+        } catch {
+            print(error)
+        }
+    }
+    
+}
+
+
+
+
--- BookMapProject/ViewController.swift
+++ BookMapProject/ViewController.swift
@@ -6,35 +6,49 @@
 //
 
 import UIKit
+
 import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    let bookStoreList: [String] = ["최인아책방", "땡스북스", "유어마인드", "스토리지북앤필름", "스페인책방", "이후북스"]
+    var location: [Int] = []
+    var locationList: [[Int]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchPlace(query: "최인아책방")
+        
+//        for num in 0...bookStoreList.count - 1 {
+//            searchPlace(query: bookStoreList[num])
+//        }
+        let apple: dummyData = dummyData()
+        apple.decode()
+        
     }
     
-    func searchPlace(query: String) {
-        let urlString = "\(EndPoint.placeURL)query=\(query)"
-        let headers: HTTPHeaders = ["X-Naver-Client-Id": APIKey.clientID, "X-Naver-Client-Secret": APIKey.clientSecret]
-        let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-        let url = URL(string: encodedString)!
-        
-        
-        AF.request(url, method: .get, headers: headers).validate().responseData { response in
-            switch response.result {
-                    case .success(let value):
-                        let json = JSON(value)
-                        print("JSON: \(json)")
-
-                       
-                    case .failure(let error):
-                        print(error)
-                    }
-                }
-    }
+//    func searchPlace(query: String) {
+//        let urlString = "\(EndPoint.placeURL)query=\(query)"
+//        let headers: HTTPHeaders = ["X-Naver-Client-Id": APIKey.clientID, "X-Naver-Client-Secret": APIKey.clientSecret]
+//        let encodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+//        let url = URL(string: encodedString)!
+//
+//
+//        AF.request(url, method: .get, headers: headers).validate().responseData { response in
+//            switch response.result {
+//                    case .success(let value):
+//
+//                        let json = JSON(value)
+//                        print("JSON: \(json)")
+//
+//
+//                    case .failure(let error):
+//                        print(error)
+//                    }
+//                }
+//    }
+    
+    
     
 
 
