8277a0a243ae34157e70467f9befa3e9b6b67ef5
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-09-15 22:02:58 +0900

    detailViewController 내 CollectionView Layout 잡기

---
+++ BookMapProject/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  BookMapProject
+//
+//  Created by 신승아 on 2022/09/15.
+//
+
+import UIKit
+
+import SnapKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static var identifier: String {
+        return "CollectionViewCell"
+    }
+}
--- BookMapProject/DetailViewController.swift
+++ BookMapProject/DetailViewController.swift
@@ -36,11 +36,15 @@ class DetailViewController: UIViewController {
         view.backgroundColor = .brown
         return view
     }()
-//
-//    var collectionView: UICollectionView = {
-//        let view = UICollectionView()
-//        return view
-//    }()
+
+    var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.backgroundColor = .orange
+        return view
+    }()
     
     override func viewDidLoad() {
         configureUI()
@@ -51,11 +55,15 @@ class DetailViewController: UIViewController {
             labelList[num].text = storeInfoList[num]
         }
         
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        
     }
     
     
     func configureUI() {
-        [storeName, storeAddress, storeTime, storeLink].forEach {
+        [storeName, storeAddress, storeTime, storeLink, collectionView].forEach {
             view.addSubview($0)
         }
         
@@ -87,10 +95,48 @@ class DetailViewController: UIViewController {
             make.height.equalTo(40)
         }
         
-        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(storeLink.snp.bottom).offset(15)
+            make.leadingMargin.equalTo(10)
+            make.trailingMargin.equalTo(-10)
+            make.height.equalTo(245)
+        }
         
         
     }
     
     
+}
+
+extension DetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as! CollectionViewCell
+        cell.backgroundColor = .yellow
+        return cell
+    }
+    
+    
+}
+
+extension DetailViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3 - 1
+        let size = CGSize(width: width, height: width)
+        return size
+    }
 }
