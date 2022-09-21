ba9ae7a3e531ff246a8a3f6b84af6cb13ed36dee
seongahshin <90595710+seongahshin@users.noreply.github.com>
2022-09-21 18:37:22 +0900

    FSCalander 날짜 클릭시 ContentView 뜨는 액션 추가

--- BookMapProject/AppDelegate.swift
+++ BookMapProject/AppDelegate.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-@main
+import IQKeyboardManagerSwift
+
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
+        
+        
         // Override point for customization after application launch.
         return true
     }
@@ -33,4 +43,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
 
--- BookMapProject/ViewController/CalendarViewController.swift
+++ BookMapProject/ViewController/CalendarViewController.swift
@@ -12,28 +12,88 @@ import SnapKit
 
 class CalendarViewController: UIViewController {
     
-    var calendar = FSCalendar()
     var events: [Date] = []
+    
+    var calendar = FSCalendar()
+    
+    var tutorialLabel: UILabel = {
+        let view = UILabel()
+        view.textAlignment = .center
+        view.text = "날짜를 클릭해서 독립서점 방문기록을 남겨보세요 :)"
+        view.font = UIFont(name: FontManager.GangWonLight, size: 13)
+        view.textColor = .lightGray
+        return view
+    }()
+    
+    var contentView: UIView = {
+        let view = UIView()
+        view.isHidden = true
+        view.backgroundColor = .brown
+        return view
+    }()
+    
+    var memoTitle: UITextField = {
+        let view = UITextField()
+        view.backgroundColor = .darkGray
+        return view
+    }()
+    
+    var memoContent: UITextView = {
+        let view = UITextView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
     
     override func viewDidLoad() {
         view.backgroundColor = backgroundColor
         configureUI()
         calendarDesign()
+        calendar.delegate = self
     }
     
     func configureUI() {
         
-        [calendar].forEach {
+        [calendar,tutorialLabel, contentView].forEach {
             view.addSubview($0)
         }
+        
+        [memoTitle, memoContent].forEach {
+            contentView.addSubview($0)
+        }
+        
         
         calendar.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.left.right.equalToSuperview()
-            make.height.equalTo(UIScreen.main.bounds.height / 2)
+            make.height.equalTo(UIScreen.main.bounds.height / 2 - 20)
+        }
+        
+        tutorialLabel.snp.makeConstraints { make in
+            make.top.equalTo(calendar.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.top.equalTo(calendar.snp.bottom)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.equalToSuperview()
+        }
+        
+        memoTitle.snp.makeConstraints { make in
+            make.top.left.right.equalTo(contentView).inset(15)
+            make.height.equalTo(40)
+        }
+        
+        memoContent.snp.makeConstraints { make in
+            make.left.right.bottom.equalTo(contentView).inset(15)
+            make.top.equalTo(memoTitle.snp.bottom).offset(15)
         }
         
     }
+}
+
+extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource, FSCalendarDelegateAppearance {
     
     func calendarDesign() {
         
@@ -50,4 +110,11 @@ class CalendarViewController: UIViewController {
         
         calendar.appearance.headerMinimumDissolvedAlpha = 0.0
     }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
+        tutorialLabel.isHidden = true
+        contentView.isHidden = false
+    }
 }
+
